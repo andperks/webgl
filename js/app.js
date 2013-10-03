0e89dabe75dbe94ff478111386191d283204da82
Andy Perkins <perksofthejob@gmail.com>
2013-10-03 15:38:46 +0100

    restructed to externalise JS, added texture images

---
+++ js/app.js
@@ -0,0 +1,62 @@
+var app =
+{
+	camera : null,
+	scene : null,
+	renderer : null,
+	mesh : null,
+
+	init : function( )
+	{
+
+		renderer = new THREE.WebGLRenderer( );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		document.body.appendChild( renderer.domElement );
+
+		//
+
+		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+		camera.position.z = 400;
+
+		scene = new THREE.Scene( );
+
+		var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+		
+
+		var texture = THREE.ImageUtils.loadTexture( 'textures/glass1.png' );
+		texture.anisotropy = renderer.getMaxAnisotropy( );
+
+		var material = new THREE.MeshBasicMaterial(
+		{
+			map : texture
+		} );
+
+		mesh = new THREE.Mesh( geometry, material );
+		scene.add( mesh );
+
+		//
+
+		window.addEventListener( 'resize', app.onWindowResize, false );
+
+		app.animate( );
+	},
+
+	onWindowResize : function( )
+	{
+
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix( );
+
+		renderer.setSize( window.innerWidth, window.innerHeight );
+
+	},
+
+	animate : function( )
+	{
+		requestAnimationFrame( app.animate );
+
+		mesh.rotation.x += 0.005;
+		mesh.rotation.y += 0.01;
+
+		renderer.render( scene, camera );
+	}
+};
