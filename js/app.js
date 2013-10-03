7127c7196ddd17c556961f1fd4f52dc390f25b4f
Andy Perkins <perksofthejob@gmail.com>
2013-10-03 17:22:26 +0100

    iffy transparency

--- js/app.js
+++ js/app.js
@@ -14,29 +14,50 @@ var app =
 
 		//
 
+		scene = new THREE.Scene( );
+
+		// LIGHTS!
+		var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+		directionalLight.position.set( 2, 2, 3 );
+		scene.add( directionalLight );
+
+		var ambiLight = new THREE.AmbientLight( 0x404040 );
+		// soft white light
+		scene.add( ambiLight );
+
+		// var hemiLight = new THREE.HemisphereLight(0xFFFFFF, 0x666666, 1 );
+		// scene.add( hemiLight ) ;
+
+		// var light = new THREE.PointLight( 0xffffff, 1, 100 );
+		// light.position.set( 0, 0, 0 );
+		// scene.add( light );
+
+		// CAMERA!
 		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
 		camera.position.z = 400;
 
-		scene = new THREE.Scene( );
-
 		var geometry = new THREE.CubeGeometry( 200, 200, 200 );
-		
 
 		var texture = THREE.ImageUtils.loadTexture( 'textures/glass1.png' );
 		texture.anisotropy = renderer.getMaxAnisotropy( );
 
-		var material = new THREE.MeshBasicMaterial(
+		var material = new THREE.MeshPhongMaterial(
 		{
-			map : texture
+			map : texture,
+			transparent : true,
+			opacity : 0.4,
+			side : THREE.DoubleSide
 		} );
 
 		mesh = new THREE.Mesh( geometry, material );
 		scene.add( mesh );
 
-		//
+		// material.opacity = 0.7;
 
+		//
 		window.addEventListener( 'resize', app.onWindowResize, false );
 
+		// ACTION!
 		app.animate( );
 	},
 
@@ -58,5 +79,6 @@ var app =
 		mesh.rotation.y += 0.01;
 
 		renderer.render( scene, camera );
+		// renderer.render( scene, camera, directionalLight );
 	}
 };
