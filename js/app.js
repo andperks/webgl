381e12bba021c49c90447219187598cbb5563d3d
Andy Perkins <perksofthejob@gmail.com>
2013-10-17 15:34:45 +0100

    lights

--- js/app.js
+++ js/app.js
@@ -12,30 +12,24 @@ var app =
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild( renderer.domElement );
 
-		//
-
 		scene = new THREE.Scene( );
 
 		// LIGHTS!
-		var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-		directionalLight.position.set( 2, 2, 3 );
-		scene.add( directionalLight );
+        var light = new THREE.PointLight( 0xffffff, 1, 100 );
+        light.position.set( 300, 300, 300 );
+        scene.add( light );
 
 		var ambiLight = new THREE.AmbientLight( 0x404040 );
 		// soft white light
 		scene.add( ambiLight );
 
-		// var hemiLight = new THREE.HemisphereLight(0xFFFFFF, 0x666666, 1 );
-		// scene.add( hemiLight ) ;
-
-		// var light = new THREE.PointLight( 0xffffff, 1, 100 );
-		// light.position.set( 0, 0, 0 );
-		// scene.add( light );
 
 		// CAMERA!
 		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
 		camera.position.z = 400;
 
+
+        // TEXTURES!
 		var geometry = new THREE.CubeGeometry( 200, 200, 200 );
 
 		var texture = THREE.ImageUtils.loadTexture( 'textures/glass1.png' );
@@ -43,18 +37,12 @@ var app =
 
 		var material = new THREE.MeshPhongMaterial(
 		{
-			map : texture,
-			transparent : true,
-			opacity : 0.4,
-			side : THREE.DoubleSide
+			map : texture
 		} );
 
 		mesh = new THREE.Mesh( geometry, material );
 		scene.add( mesh );
 
-		// material.opacity = 0.7;
-
-		//
 		window.addEventListener( 'resize', app.onWindowResize, false );
 
 		// ACTION!
