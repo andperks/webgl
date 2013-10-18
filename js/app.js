e5391fb3593615408b7c8bfacf60de1d8e6a6a82
Andy Perkins <perksofthejob@gmail.com>
2013-10-18 17:07:49 +0100

    working except position

--- js/app.js
+++ js/app.js
@@ -26,24 +26,33 @@ var app =
         // add to the scene
         scene.add(pointLight);
 
-//        var ambiLight = new THREE.AmbientLight(0x404040);
-//        // soft white light
-//        scene.add(ambiLight);
+        var ambiLight = new THREE.AmbientLight(0x404040);
+        // soft white light
+        scene.add(ambiLight);
 
         // CAMERA!
-        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-        camera.position.z = 400;
+        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+        camera.position.z = 800;
 
         // STUFF!
 //        var geometry = new THREE.CubeGeometry(200, 200, 200);
 
         var geometry = new THREE.SphereGeometry( 200, 20, 20) ;
+        var mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({ color : Math.random() * 0xFF0000 }) );
 
-        trixelate( geometry ) ;
+
+//        scene.add( mesh );
+
+        trixelate( mesh ) ;
+
+//        console.log( trixels[ 0 ].mesh ) ;
 
         window.addEventListener('resize', app.onWindowResize, false);
 
         // ACTION!
+
+        setTimeout( app.tween ,500) ;
+
         app.animate();
     },
 
@@ -60,15 +69,34 @@ var app =
         requestAnimationFrame(app.animate);
 
         // TODO: Loop through all items
+//        for (var i = 0, j = trixels.length; i < j; i++)
+//        {
+//            var t = trixels[ i ] ;
+//            t.mesh.rotation.x += Math.random() * 0.01 ;
+//            t.mesh.rotation.y += Math.random() * 0.01 ;
+//            t.mesh.rotation.z += Math.random() * 0.01 ;
+//        }
+
+        //camera.position.z += 5;
+
+//        trixels[ 0 ].mesh.rotation.z += Math.random() * 0.01;
+
+        renderer.render(scene, camera);
+    },
+
+    tween : function( )
+    {
         for (var i = 0, j = trixels.length; i < j; i++)
         {
             var t = trixels[ i ] ;
-                t.mesh.rotation.x += Math.random() * 0.01;
-                t.mesh.rotation.y += Math.random() * 0.01;
-                t.mesh.rotation.z += Math.random() * 0.01;
-        }
+//            TweenMax.to(t.mesh.position, 2, { x : (Math.random() * 400) - 200,
+//                                              y : (Math.random() * 400) - 200,
+//                                              z : (Math.random() * 400) - 200 } );
 
-        renderer.render(scene, camera);
+            TweenMax.to(t.mesh.position, 2, { x : t.homeX,
+                                              y : t.homeY,
+                                              z : t.homeZ } );
+        }
     }
 };
 
@@ -92,13 +120,15 @@ var app =
  scene.add(triangle)
  */
 
-function trixelate(geometry) {
+function trixelate( mesh ) {
 
     // send a piece of geometry
-    var ts = geometry.faces;       // triangles
-    var vs = geometry.vertices;    // vertices
+    var ts = mesh.geometry.faces;       // triangles
+    var vs = mesh.geometry.vertices;    // vertices
 
     var gs = [ ];                  //list of triangles
+
+    var triangles = [] ;
 
     // break it into component triangles
     for (var i = 0, j = ts.length; i < j; i++) {
@@ -109,29 +139,78 @@ function trixelate(geometry) {
         var vB = vs[ t.b ];   // vertex
         var vC = vs[ t.c ];   // vertex
 
-        var g = new THREE.Geometry( ) ;
-            g.vertices.push( vA ) ;
-            g.vertices.push( vB ) ;
-            g.vertices.push( vC ) ;
-            g.faces.push( new THREE.Face3( 1, 0, 2) ); // must be added counter-clockwise
+        var verts = { a: vA , b : vB, c : vC } ;
 
-//        var mat = new THREE.MeshLambertMaterial(
-//        {
-//            color: Math.random() * 0xFFFFFF
-//        });
+        triangles.push( verts ) ;
 
-        var mat = new THREE.MeshBasicMaterial({ color : Math.random() * 0xFF0000 });
-        var mesh = new THREE.Mesh( g, mat );
-        var trixel = new Trixel( mesh ) ;
+//        var g = new THREE.Geometry( ) ;
+//            g.vertices.push( vA ) ;
+//            g.vertices.push( vB ) ;
+//            g.vertices.push( vC ) ;
+//            g.faces.push( new THREE.Face3( 0, 2, 1) ); // must be added counter-clockwise
+//
+////        var mat = new THREE.MeshLambertMaterial(
+////        {
+////            color: Math.random() * 0xFFFFFF
+////        });
+//
+//        var mat = new THREE.MeshPhongMaterial({ color : Math.random() * 0xFF0000 }) ;
+//
+//        var tm = new THREE.Mesh( g, mat );
+//
+//        //centroid
+//        var cx = (vA.x + vB.x + vC.x) / 3 ;
+//        var cy = (vA.y + vB.y + vC.y) / 3 ;
+//        var cz = (vA.z + vB.z + vC.z) / 3 ;
+//
+//        var trixel = new Trixel( tm ) ;
+//
+//        trixels.push( trixel ) ;
+//
+//        scene.add( trixel.mesh );
+//
+//        tm.position.x = cx ;
+//        tm.position.y = cy ;
+//        tm.position.z = cz ;
+
+
+    }
+
+    for (var i = 0, j = triangles.length; i < j; i++)
+    {
+        var currentTriangle = triangles [ i ] ;
+
+        var g = new THREE.Geometry( );
+//            g.vertices.push( currentTriangle.a ) ;
+//            g.vertices.push( currentTriangle.b ) ;
+//            g.vertices.push( currentTriangle.c ) ;
+            g.faces.push( new THREE.Face3( 0, 2, 1) ); // must be added counter-clockwise
+
+        var mat = new THREE.MeshBasicMaterial({ color : Math.random() * 0xFF0000 }) ;
+
+        var tm = new THREE.Mesh( g, mat );
+
+        var trixel = new Trixel( tm ) ;
+
+        // centroid
+        var cx = (currentTriangle.a.x + currentTriangle.b.x + currentTriangle.c.x) / 3 ;
+        var cy = (currentTriangle.a.y + currentTriangle.b.y + currentTriangle.c.y) / 3 ;
+        var cz = (currentTriangle.a.z + currentTriangle.b.z + currentTriangle.c.z) / 3 ;
 
         trixels.push( trixel ) ;
 
+        tm.position.x = 0 ;
+        tm.position.y = 0 ;
+        tm.position.z = 0 ;
+
+        console.log( trixel.mesh ) ;
         scene.add( trixel.mesh );
     }
-
 }
 
 function Trixel( mesh )
 {
+
     this.mesh = mesh ;
+
 }
